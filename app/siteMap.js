ba70228444d4473c2aed905871c1a0eae2c805c4
Darlin Manuel <darlinlvaldez@gmail.com>
2026-06-28 15:24:14 -0400

    Ahora si que si

--- app/siteMap.js
+++ app/siteMap.js
@@ -1,8 +1,41 @@
-export default function sitemap() {
-  return [
-    {
-      url: "https://404news.up.railway.app",
-      lastModified: new Date(),
-    },
+import forSeo from "@/server/services/news/seo";
+
+import config from "../config";
+
+export default async function sitemap() {
+
+    const [news, categories, authors] = await Promise.all([
+        forSeo.getNews(),
+        forSeo.getCategories(),
+        forSeo.getAuthors(),
+    ]);
+
+    return [
+
+        {
+        url: config.PORTAL,
+        lastModified: new Date(),
+        changeFrequency: "hourly",
+        priority: 1,
+        },
+
+        ...news.map((article) => ({
+        url: `${config.PORTAL}/news/news-details/${article.slug}`,
+        lastModified: article.updated_at,
+        changeFrequency: "daily",
+        priority: 0.9,
+        })),
+
+        ...categories.map((category) => ({
+        url: `${config.PORTAL}/news/section/${category.slug}`,
+        changeFrequency: "daily",
+        priority: 0.8,
+        })),
+
+        ...authors.map((author) => ({
+        url: `${config.PORTAL}/news/author/${author.slug}`,
+        changeFrequency: "weekly",
+        priority: 0.7,
+    })),
   ];
 }
