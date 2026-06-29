99f8dfe33484edbbc8f57ef1354ac590343606fd
Darlin Manuel <darlinlvaldez@gmail.com>
2026-06-29 14:55:15 -0400

    Fecha reparada

--- server/controllers/news/redis.js
+++ server/controllers/news/redis.js
@@ -32,8 +32,8 @@ export default async function incrementView(slug, ip, visitorId) {
     await redis.hSet(countryKey, {
       views: 1,
       name: country.name,
-      created_at: new Date().toISOString()
-    });
+      created_at: new Date().toISOString().slice(0, 19) .replace("T", " ")
+      });
   } else {
     await redis.hIncrBy(countryKey, "views", 1);
   }
